a9d06239e06601d34beb8f2c5431bfac878acd81
PritamDutta <webdevpritam7498@gmail.com>
2023-10-16 11:35:04 +0600

    update

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zynq1cd.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,22 +27,6 @@ async function run() {
   try {
     await client.connect();
 
-    const menuCollection = client.db("bistroDb").collection("menu");
-    const reviewCollection = client.db("bistroDb").collection("reviews");
-    const cartCollection = client.db("bistroDb").collection("carts");
-    // get menu data from collection
-    app.get("/menu", async (req, res) => {
-      const result =  await menuCollection.find().toArray();
-      res.send(result);
-    });
-    // get review data from collection
-    app.get('/reviews', async(req, res)=>{
-      const result = await reviewCollection.find().toArray();
-      res.send(result)
-    })
-
-
-
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -50,6 +37,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+
+
+
+
 app.get("/", (req, res) => {
   res.send("welcome to bistro boss");
 });
@@ -57,3 +48,26 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`listening on port, ${port}`);
 });
+
+
+// const menuCollection = client.db("bistroDb").collection("menu");
+// const reviewCollection = client.db("bistroDb").collection("reviews");
+// const cartCollection = client.db("bistroDb").collection("carts");
+// // get menu data from collection
+// app.get("/menu", async (req, res) => {
+//   const result =  await menuCollection.find().toArray();
+//   res.send(result);
+// });
+// // get review data from collection
+// app.get('/reviews', async(req, res)=>{
+//   const result = await reviewCollection.find().toArray();
+//   res.send(result)
+// })
+
+// //insert cart data into  cart collection
+// app.post('/carts', async (req, res) =>{
+//   const item = req.body;
+//   console.log(item);
+//   const result = await cartCollection.insertOne(item);
+//   res.send(result)
+// })
