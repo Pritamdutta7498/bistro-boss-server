8347ab9242cbb213c145be6078e0b9a1d310e0f8
PritamDutta <webdevpritam7498@gmail.com>
2023-11-14 16:24:00 +0600

    creating the single delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -58,6 +58,13 @@ app.post('/carts', async (req, res) =>{
   const item = req.body;
   // console.log(item);
   const result = await cartCollection.insertOne(item);
+  res.send(result)
+})
+//delete cart item from cart collection
+app.delete('/carts/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await cartCollection.deleteOne(query);
   res.send(result)
 })
 
